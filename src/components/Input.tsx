7c810742901ded26835c8b605f2640a2f5865bd4
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-05-04 19:28:46 -0300

    refactor: sign up page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,32 @@
 interface Props {
   label: string
+  alt?: boolean
   [props: string]: unknown
 }
-export const Input = ({ label, ...props }: Props) => {
+export const Input = ({ label, alt, ...props }: Props) => {
   return (
-    <div className="flex flex-row items-center my-2">
-      <label
-        className="w-28 py-2 px-2 border-2 border-gray-300 rounded rounded-r-none bg-gray-50 border-r-0 text-gray-400 whitespace-nowrap"
-        htmlFor={label}
-      >
-        {label}
-      </label>
+    <div
+      className={`w-full flex flex-row items-center my-2 ${alt && 'flex-col'}`}
+    >
+      {alt ? (
+        <label
+          className="w-full block font-medium text-gray-700"
+          htmlFor={label}
+        >
+          {label}
+        </label>
+      ) : (
+        <label
+          className="px-2 py-2 text-gray-400 border-2 border-r-0 border-gray-300 rounded rounded-r-none bg-gray-50 whitespace-nowrap"
+          htmlFor={label}
+        >
+          {label}
+        </label>
+      )}
       <input
-        className="w-full p-2 border-2 border-gray-300 rounded-none rounded-r-md focus:ring-1 focus:ring-blue-500 focus:border-blue-500 transition-all shadow-sm"
+        className={`flex w-full p-2 border-2 border-gray-300 ${
+          !alt ? 'rounded-none' : 'rounded-l-md'
+        } rounded-r-md focus:ring-1 focus:ring-blue-500 focus:border-blue-500 transition-all shadow-sm`}
         {...props}
       />
     </div>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,14 +4,27 @@ import { Layout } from '../components/Layout'
 
 const SignUpPage = () => (
   <Layout>
-    <form className="flex flex-col w-4/5 xl:w-2/5">
-      <h1 className="mb-12 text-5xl font-bold">Create new account</h1>
-      <Input label="Full name" />
-      <Input label="E-mail" />
-      <Input label="CPF" />
-      <Input label="Password" />
-      <Input label="Confirm password" />
-      <Button label="Sign Up" style={{ marginTop: '1em' }} />
+    <form className="flex flex-col px-4 xl:w-2/5">
+      <h1 className="text-lg font-bold text-blue-500 leading-6 mb-1">
+        Sign Up
+      </h1>
+      <span className="mb-6 text-sm text-gray-600">
+        By signing up you'll be agreeing with whatever terms of usage i wish.
+        Thank you.
+      </span>
+      <div className="justify-between lg:flex lg:flex-row lg:space-x-4">
+        <Input alt label="Full name" style={{ width: '100%' }} />
+        <Input alt label="E-mail" />
+      </div>
+      <span className="mt-1 text-sm text-gray-600">
+        Please provide a valid brazillian CPF number.
+      </span>
+      <Input label="CPF" placeholder="000.000.000-00" />
+      <div className="justify-between lg:flex lg:flex-row lg:space-x-4">
+        <Input alt label="Password" />
+        <Input alt label="Confirm password" />
+      </div>
+      <Button label="Confirm" style={{ marginTop: '1em' }} />
     </form>
   </Layout>
 )
