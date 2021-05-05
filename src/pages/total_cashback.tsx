701fddb264159f8d57afa041d4b4dd34c87fca0e
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-05-05 18:34:48 -0300

    refact: total_cashback page

--- src/pages/total_cashback.tsx
+++ src/pages/total_cashback.tsx
@@ -2,9 +2,14 @@ import { Layout } from '../components/Layout'
 
 const CashbackPage = () => (
   <Layout>
-    <div className="flex flex-col">
-      <h1 className="text-4xl font-bold">Total cashback: </h1>
-      <span className="text-2xl font-bold text-blue-500">R$293923,00</span>
+    <div className="flex flex-col px-4 xl:w-2/5 xl:px-24">
+      <h1 className="text-lg font-bold text-blue-500 leading-6">
+        Total cashback
+      </h1>
+      <span className="mt-1 mb-6 text-sm text-gray-600">
+        Here you'll see the amount of cashback you acumulated over the years.
+      </span>
+      <span className="text-3xl text-green-500 font-light">R$12323,99</span>
     </div>
   </Layout>
 )
