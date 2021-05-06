8f3019743497d36bcc06ce20d9a89b7d06dc1cee
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-05-06 19:38:53 -0300

    chore: revert dev env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 export const API_URL = 'http://localhost:3000'
-export const DEV_URL = 'http://192.168.1.7:3000'
+// export const DEV_URL = 'http://192.168.1.7:3000'
 
 export const fetcher = async (endpoint: string) => {
-  // const { href } = new URL(endpoint, API_URL)
-  const { href } = new URL(`/api/${endpoint}`, DEV_URL)
+  const { href } = new URL(`/api/${endpoint}`, API_URL)
   return fetch(href).then((res) => res.json())
 }
