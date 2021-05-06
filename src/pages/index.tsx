a9fd0fe70c2a39355149ffeeadde81b88de9722b
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-05-06 19:56:23 -0300

    fix: redirect index to login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import { useRouter } from 'next/dist/client/router'
+import { useEffect } from 'react'
 import { Layout } from '../components/Layout'
 
-const IndexPage = () => <Layout></Layout>
+const IndexPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/login')
+  }, [])
+
+  return <Layout>you are being redirected....</Layout>
+}
 
 export default IndexPage
